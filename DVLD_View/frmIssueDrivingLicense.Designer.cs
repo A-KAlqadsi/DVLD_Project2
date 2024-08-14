f1d10d931f3beda867bd4ff8b7c15dff55a07060
A-KAlqadsi <abdulkareemalqa@gmail.com>
2024-08-14 18:33:24 +0300

    completing the desing form of frmIssueDrivingLicense

--- DVLD_View/frmIssueDrivingLicense.Designer.cs
+++ DVLD_View/frmIssueDrivingLicense.Designer.cs
@@ -28,20 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmIssueDrivingLicense));
+            this.ctrlApplicationCard1 = new DVLD_View.ctrlApplicationCard();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnIssue = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
+            // 
+            // ctrlApplicationCard1
+            // 
+            this.ctrlApplicationCard1.BackColor = System.Drawing.Color.White;
+            this.ctrlApplicationCard1.Location = new System.Drawing.Point(11, 20);
+            this.ctrlApplicationCard1.Name = "ctrlApplicationCard1";
+            this.ctrlApplicationCard1.Size = new System.Drawing.Size(1037, 450);
+            this.ctrlApplicationCard1.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(133, 483);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(39, 30);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 99;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(187, 483);
+            this.txtNotes.Multiline = true;
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(856, 114);
+            this.txtNotes.TabIndex = 98;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(43, 483);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 25);
+            this.label6.TabIndex = 97;
+            this.label6.Text = "Notes:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.White;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Image = ((System.Drawing.Image)(resources.GetObject("btnClose.Image")));
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(761, 604);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(138, 43);
+            this.btnClose.TabIndex = 101;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // btnIssue
+            // 
+            this.btnIssue.BackColor = System.Drawing.Color.White;
+            this.btnIssue.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnIssue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIssue.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIssue.Image = ((System.Drawing.Image)(resources.GetObject("btnIssue.Image")));
+            this.btnIssue.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIssue.Location = new System.Drawing.Point(905, 603);
+            this.btnIssue.Name = "btnIssue";
+            this.btnIssue.Size = new System.Drawing.Size(138, 43);
+            this.btnIssue.TabIndex = 100;
+            this.btnIssue.Text = "Issue";
+            this.btnIssue.UseVisualStyleBackColor = false;
             // 
             // frmIssueDrivingLicense
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1191, 514);
+            this.ClientSize = new System.Drawing.Size(1068, 667);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnIssue);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.txtNotes);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ctrlApplicationCard1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmIssueDrivingLicense";
-            this.Text = "frmIssueDrivingLicense";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Issue Driving License";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private ctrlApplicationCard ctrlApplicationCard1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtNotes;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnIssue;
     }
 }
