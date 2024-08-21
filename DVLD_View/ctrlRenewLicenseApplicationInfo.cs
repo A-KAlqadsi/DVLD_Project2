c3c7263095d375274368499e4b55a17206e28447
A-KAlqadsi <abdulkareemalqa@gmail.com>
2024-08-21 20:27:25 +0300

    [WIP] adding the design for ctrlRenewLicenseApp

---
+++ DVLD_View/ctrlRenewLicenseApplicationInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DVLD_Business;
+
+namespace DVLD_View
+{
+    public partial class ctrlRenewLicenseApplicationInfo : UserControl
+    {
+        private int appTypeID = 2; //  Renew license application type
+
+        public int AppTypeID { get { return appTypeID; } private set { } }
+
+        public ctrlRenewLicenseApplicationInfo()
+        {
+            InitializeComponent();
+        }
+    }
+}
