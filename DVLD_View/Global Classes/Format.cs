e49c4598fcacd0a59555d7bed51ad06b533e62ed
A-KAlqadsi <abdulkareemalqa@gmail.com>
2024-10-08 23:53:08 +0300

    adding the Global Classes folder with classes for validation, Format, Global, Util

---
+++ DVLD_View/Global Classes/Format.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_View.Globals
+{
+	public class Format
+	{
+		public static string DateToShort( DateTime date )
+		{
+			return date.ToString("dd/MMM/yyyy");
+		}
+	}
+}
